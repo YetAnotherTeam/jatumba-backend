7dbe7244ae8f986d8c3b68c83943bc9d9e5f914f
Andrey Bondar <Alanai1995@gmail.com>
2016-02-28 20:13:21 +0300

    one more fix

--- api/auth/authentication.py
+++ api/auth/authentication.py
@@ -9,13 +9,13 @@ from api.models import Session
 class TokenAuthentication(BaseAuthentication):
     def authenticate(self, request):
         token = request.META.get('HTTP_TOKEN')
-        print(token)
         if not token:
             return None
-        try:
-            session = Session.objects.filter(access_token=token).first()
-        except Session.DoesNotExist:
+
+        session = Session.objects.filter(access_token=token).first()
+        if session is None:
             raise exceptions.AuthenticationFailed('No such token')
-        if session.time - time.time() > 3600:
-            return None
+
+        if time.time() - session.time > 3600:
+            raise exceptions.AuthenticationFailed('Token expired')
         return session.user, session.access_token
