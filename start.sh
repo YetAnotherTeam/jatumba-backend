5e3437bd9cd0ffb3b57def299ec6cf8a60acab6a
KhasanovBI <afti@yandex.ru>
2016-03-13 18:06:06 +0300

    Добавил настройки серверов

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git pull
 killall -9 gunicorn
-gunicorn TPD.wsgi:application &
+gunicorn TPD.wsgi:application --access-logfile /home/bulat/log/access.log --error-logfile /home/bulat/log/error.log --log-level debug --bind=unix:/tmp/gunicorn.sock &
