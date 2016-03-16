8e99f6ddb785e905e42cbc615b66509454f8722f
KhasanovBI <afti@yandex.ru>
2016-03-17 00:05:26 +0300

    Поменял миграции

--- TPD/settings.py
+++ TPD/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jatumba',
-        'USER': 'postgres',
+        'USER': 'jatumba',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
