16fd733ed6dc0c314810dfb51669221a53013332
khasanovbi <afti@yandex.ru>
2016-04-17 22:15:29 +0000

    Добавил остановку запущенных процессов в скрипт запуска

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+/home/jatumba/.virtualenvs/circus/bin/circusctl stop
+ps aux | grep -i circus[d] | awk {'print $2'} | xargs kill -9
 /home/jatumba/.virtualenvs/circus/bin/circusd circus/circus.ini --daemon
