c1b655eed0269c754ce7fce26859755366ea14ef
khasanovbi <afti@yandex.ru>
2016-04-25 15:34:57 +0000

    Небольшие изменения circus

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 /home/jatumba/.virtualenvs/circus/bin/circusctl stop
 ps aux | grep -i circus[d] | awk {'print $2'} | xargs kill -9
 /home/jatumba/.virtualenvs/circus/bin/circusd circus/circus.ini --daemon
