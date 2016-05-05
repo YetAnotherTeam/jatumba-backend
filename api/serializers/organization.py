90c09897ad1e8f75a23a529cb1fc71933ca779bb
b.khasanov <afti@yandex.ru>
2016-05-05 05:58:50 +0300

    Добавил время версии композиции, пока без порядка по версии

--- api/serializers/organization.py
+++ api/serializers/organization.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
 from api.models import *
+from api.serializers import CompositionSerializer
 from api.serializers.auth import UserSerializer
-from api.serializers.dictionary import InstrumentSerializer
 from utils.django_rest_framework.fields import SerializableRelatedField
 from utils.django_rest_framework.serializers import DynamicFieldsMixin
 
@@ -35,9 +35,18 @@ class MemberSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
 class UserRetrieveSerializer(UserSerializer):
     members = MemberSerializer(required_fields=('id', 'band'), many=True)
     instruments = serializers.PrimaryKeyRelatedField(queryset=Instrument.objects.all(), many=True)
+    compositions = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'phone', 'vk_profile', 'fb_profile',
-                  'members', 'instruments')
+                  'members', 'instruments', 'compositions')
         extra_kwargs = {'vk_profile': {'read_only': True}, 'fb_profile': {'read_only': True}}
+
+    def get_compositions(self, user):
+        # TODO добавить порядок
+        return CompositionSerializer(
+            Composition.objects.filter(band__members__user=user.id).order_by(),
+            required_fields=('id', 'latest_version', 'name', 'band', 'genres'),
+            many=True
+        ).data
