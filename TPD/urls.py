af1671e9629b15c2f735f9023c31a2addc50fa6d
b.khasanov <afti@yandex.ru>
2016-05-09 22:48:01 +0300

    добавил drfdocs

--- TPD/urls.py
+++ TPD/urls.py
@@ -22,6 +22,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+    url(r'^docs/', include('rest_framework_docs.urls')),
 ]
 
 if settings.DEBUG:
