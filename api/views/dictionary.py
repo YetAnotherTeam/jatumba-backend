9c1deab934d64ef3c315afbe7056b4b7b8c79316
b.khasanov <afti@yandex.ru>
2016-08-09 10:00:46 +0300

    Добавил порядок инструментов.

--- api/views/dictionary.py
+++ api/views/dictionary.py
@@ -5,6 +5,6 @@ from api.serializers import InstrumentListItemSerializer
 
 
 class InstrumentViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    queryset = Instrument.objects.prefetch_related('sounds')
+    queryset = Instrument.objects.order_by('name').prefetch_related('sounds')
     serializer_class = InstrumentListItemSerializer
     pagination_class = None
